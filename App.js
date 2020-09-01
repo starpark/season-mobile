6233653d6db8ac31d2f6adf29990ac8d2031bbd3
starpark <62285975+starpark@users.noreply.github.com>
2020-09-01 18:26:07 +0900

    add modules

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Provider as PaperProvider } from "react-native-paper";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, Season!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Hello, Season!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
