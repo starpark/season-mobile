75fb2c74029a335d03ac2ba1b91d4da1d92b8340
starpark <62285975+starpark@users.noreply.github.com>
2020-11-28 01:45:03 +0900

    edit provider

--- App.js
+++ App.js
@@ -1,16 +1,13 @@
-import React from "react";
+import * as React from "react";
 import RootScreen from "./App/Screens/RootScreen";
 import { Provider as StoreProvider } from "react-redux";
-import { Provider as PaperProvider } from "react-native-paper";
 import rootReducer from "./App/Redux/Reducers";
 import { createStore } from "redux";
 
 export default function App() {
   return (
     <StoreProvider store={createStore(rootReducer)}>
-      <PaperProvider>
-        <RootScreen />
-      </PaperProvider>
+      <RootScreen />
     </StoreProvider>
   );
 }
