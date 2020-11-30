a0fe1aaf432643be98709528d86846677f168e87
starpark <62285975+starpark@users.noreply.github.com>
2020-11-30 21:23:15 +0900

    remove unused modules

--- App/Screens/Course/CourseWeekly.js
+++ App/Screens/Course/CourseWeekly.js
@@ -11,8 +11,6 @@ import {
 } from "react-native-paper";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import Global from "../../Styles/GlobalStyles";
-import * as FileSystem from "expo-file-system";
-import * as DocumentPicker from "expo-document-picker";
 
 const theme = {
   ...DefaultTheme,
@@ -31,9 +29,6 @@ const CourseWeekly = () => {
   const [visible, setVisible] = React.useState(false);
   const [type, setType] = React.useState("");
   const [item, setItem] = React.useState([]);
-  const [state, setState] = React.useState({ open: false });
-
-  const route = useRoute();
   const navigation = useNavigation();
 
   const showModal = ({ type, item }) => {
@@ -48,91 +43,6 @@ const CourseWeekly = () => {
       lectures.push({ lectures: item.lectures, week: item.week });
     });
   }, []);
-
-  // const attachmentsDir = FileSystem.cacheDirectory + `${route.params.title}/`;
-
-  // const callback = (downloadProgress) => {
-  //   const progress =
-  //     downloadProgress.totalBytesWritten /
-  //     downloadProgress.totalBytesExpectedToWrite;
-  //   console.log(progress);
-  // };
-
-  // const ensureDirExists = async () => {
-  //   const dirInfo = await FileSystem.getInfoAsync(attachmentsDir);
-  //   if (!dirInfo.exists) {
-  //     await FileSystem.makeDirectoryAsync(attachmentsDir, {
-  //       intermediates: true,
-  //     });
-  //   }
-  // };
-
-  // const downloadFile = async (item) => {
-  //   const { uri: link, uuid } = item;
-
-  //   const splitUri = link.split("/");
-  //   const fileName = splitUri[splitUri.length - 1];
-
-  //   const fileUri =
-  //     attachmentsDir + `${route.params.title}_${uuid}_${fileName}`;
-
-  //   const downloadResumable = FileSystem.createDownloadResumable(
-  //     link,
-  //     fileUri,
-  //     {},
-  //     callback
-  //   );
-  //   try {
-  //     const result = await downloadResumable.downloadAsync();
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // };
-
-  // const ensureFileExists = async (item) => {
-  //   await ensureDirExists();
-
-  //   const { uri: link, uuid } = item;
-
-  //   const splitUri = link.split("/");
-  //   const fileName = splitUri[splitUri.length - 1];
-
-  //   const fileUri =
-  //     attachmentsDir + `${route.params.title}_${uuid}_${fileName}`;
-  //   const fileInfo = await FileSystem.getInfoAsync(fileUri);
-
-  //   if (!fileInfo.exists) {
-  //     await downloadFile(item);
-  //   }
-
-  //   return fileUri;
-  // };
-
-  // const deleteFile = async (item) => {
-  //   const { uri: link, title, uuid } = item;
-
-  //   const splitUri = link.split("/");
-  //   const fileName = splitUri[splitUri.length - 1];
-
-  //   const fileUri = attachmentsDir + `${title}_${uuid}_${fileName}`;
-
-  //   await FileSystem.deleteAsync(fileUri);
-  // };
-
-  // const getContentUri = async (item) => {
-  //   const uri = await FileSystem.getContentUriAsync(
-  //     await ensureFileExists(item)
-  //   );
-  //   console.log(FileSystem.cacheDirectory);
-
-  //   DocumentPicker.getDocumentAsync({
-  //     type: "*/*",
-  //     copyToCacheDirectory: true,
-  //   });
-
-  //   console.log(uri);
-  //   return uri;
-  // };
 
   const ItemDialog = ({ item, type, visible }) => (
     <Dialog visible={visible} onDismiss={hideDialog}>
@@ -171,7 +81,13 @@ const CourseWeekly = () => {
           <View>
             <TouchableRipple
               style={{ height: 40, justifyContent: "center", paddingLeft: 10 }}
-              onPress={() => console.log(1)}
+              onPress={() => {
+                navigation.navigate("PDF", {
+                  title: item.title,
+                  uri: item.uri,
+                });
+                hideDialog();
+              }}
             >
               <Text>보기</Text>
             </TouchableRipple>
@@ -227,7 +143,9 @@ const CourseWeekly = () => {
             {type === "assignments" && (
               <List.Icon {...props} icon="close" color="red" />
             )}
-            {type === "attachments" && <List.Icon {...props} icon="download" />}
+            {type === "attachments" && (
+              <List.Icon {...props} icon="file-pdf-outline" />
+            )}
           </View>
         )}
       />
@@ -474,8 +392,7 @@ const response = [
         uuid: "10",
         title: "string",
         description: "string",
-        uri:
-          "https://learn-ap-northeast-2-prod-fleet01-xythos.content.blackboardcdn.com/5cf774ff89eaf/3508111?X-Blackboard-Expiration=1606381200000&X-Blackboard-Signature=gLLJZwS70cGmkfjHPZ5cqBHhTpPfMR7cn3LDjXR5uk0%3D&X-Blackboard-Client-Id=513122&response-cache-control=private%2C%20max-age%3D21600&response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%27C1.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20201126T030000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=AKIAZH6WM4PLRAIZCZRQ%2F20201126%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Signature=605053304d8ab8ca7e94943790c0f03d126d399054f7676f1e30ee6934f27251",
+        uri: "http://techslides.com/demos/sample-videos/small.mp4",
         comments: [
           {
             user: {
@@ -541,7 +458,7 @@ const response = [
         title: "string",
         description: "string",
         uri:
-          "https://learn-ap-northeast-2-prod-fleet01-xythos.content.blackboardcdn.com/5cf774ff89eaf/4467972?X-Blackboard-Expiration=1606381200000&X-Blackboard-Signature=tPeA7kgLd908jjxsZ8%2FiithBKxB97OT0O8WF2iv6rLo%3D&X-Blackboard-Client-Id=513122&response-cache-control=private%2C%20max-age%3D21600&response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%27C9.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20201126T030000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=AKIAZH6WM4PLRAIZCZRQ%2F20201126%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Signature=47a3bf395e1d16f6c1df0a4c95b5e5a43709f82b776088df7623df7fc20bcff7",
+          "https://blackboard.sejong.ac.kr/bbcswebdav/pid-2456812-dt-content-rid-9152629_1/xid-9152629_1",
         comments: [
           {
             user: {
