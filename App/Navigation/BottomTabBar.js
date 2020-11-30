9cb460ae810c521072acbbfbc88e8a4e7241b6f9
starpark <62285975+starpark@users.noreply.github.com>
2020-11-30 21:19:08 +0900

    add hide function on keyboard

--- App/Navigation/BottomTabBar.js
+++ App/Navigation/BottomTabBar.js
@@ -6,11 +6,13 @@ import {
   Dimensions,
   Animated,
   StyleSheet,
+  Keyboard,
 } from "react-native";
 import { CommonActions } from "@react-navigation/native";
 
 const TabBar = ({ state, descriptors, navigation }) => {
   const [translateValue] = useState(new Animated.Value(0));
+  const [showTab, setShowTab] = useState(true);
   const totalWidth = Dimensions.get("window").width;
   const tabWidth = totalWidth / state.routes.length;
 
@@ -26,70 +28,92 @@ const TabBar = ({ state, descriptors, navigation }) => {
     animateSlider(state.index);
   }, [state.index]);
 
+  useEffect(() => {
+    Keyboard.addListener("keyboardDidShow", _keyboardDidShow);
+    Keyboard.addListener("keyboardDidHide", _keyboardDidHide);
+
+    return () => {
+      Keyboard.removeListener("keyboardDidShow", _keyboardDidShow);
+      Keyboard.removeListener("keyboardDidHide", _keyboardDidHide);
+    };
+  }, []);
+
+  const _keyboardDidShow = () => {
+    setShowTab(false);
+  };
+
+  const _keyboardDidHide = () => {
+    setShowTab(true);
+  };
+
   return (
-    <View style={[style.tabContainer, { width: totalWidth }]}>
-      <View style={{ flexDirection: "row" }}>
-        <Animated.View
-          style={[
-            style.slider,
-            {
-              transform: [{ translateX: translateValue }],
-              width: tabWidth - 20,
-            },
-          ]}
-        />
+    <View>
+      {showTab && (
+        <View style={[style.tabContainer, { width: totalWidth }]}>
+          <View style={{ flexDirection: "row" }}>
+            <Animated.View
+              style={[
+                style.slider,
+                {
+                  transform: [{ translateX: translateValue }],
+                  width: tabWidth - 20,
+                },
+              ]}
+            />
 
-        {state.routes.map((route, index) => {
-          const { options } = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name;
+            {state.routes.map((route, index) => {
+              const { options } = descriptors[route.key];
+              const label =
+                options.tabBarLabel !== undefined
+                  ? options.tabBarLabel
+                  : options.title !== undefined
+                  ? options.title
+                  : route.name;
 
-          const isFocused = state.index === index;
+              const isFocused = state.index === index;
 
-          const onPress = () => {
-            const event = navigation.emit({
-              type: "tabPress",
-              target: route.key,
-              canPreventDefault: true,
-            });
+              const onPress = () => {
+                const event = navigation.emit({
+                  type: "tabPress",
+                  target: route.key,
+                  canPreventDefault: true,
+                });
 
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name);
-            }
+                if (!isFocused && !event.defaultPrevented) {
+                  navigation.navigate(route.name);
+                }
 
-            animateSlider(index);
-          };
+                animateSlider(index);
+              };
 
-          const onLongPress = () => {
-            navigation.emit({
-              type: "tabLongPress",
-              target: route.key,
-            });
-          };
+              const onLongPress = () => {
+                navigation.emit({
+                  type: "tabLongPress",
+                  target: route.key,
+                });
+              };
 
-          return (
-            <TouchableOpacity
-              accessibilityRole="button"
-              accessibilityStates={isFocused ? ["selected"] : []}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-              style={{ flex: 1 }}
-              key={index}
-            >
-              <BottomMenuItem
-                isCurrent={isFocused}
-                iconName={label.toString()}
-              />
-            </TouchableOpacity>
-          );
-        })}
-      </View>
+              return (
+                <TouchableOpacity
+                  accessibilityRole="button"
+                  accessibilityStates={isFocused ? ["selected"] : []}
+                  accessibilityLabel={options.tabBarAccessibilityLabel}
+                  testID={options.tabBarTestID}
+                  onPress={onPress}
+                  onLongPress={onLongPress}
+                  style={{ flex: 1 }}
+                  key={index}
+                >
+                  <BottomMenuItem
+                    isCurrent={isFocused}
+                    iconName={label.toString()}
+                  />
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        </View>
+      )}
     </View>
   );
 };
