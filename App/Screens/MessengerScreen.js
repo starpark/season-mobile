377f39113d00c30bd474893b8532ce6df64b3e9e
starpark <62285975+starpark@users.noreply.github.com>
2020-11-30 21:23:53 +0900

    edit some test case

--- App/Screens/MessengerScreen.js
+++ App/Screens/MessengerScreen.js
@@ -1,46 +1,115 @@
-import * as React from "react";
-import { View, Text, ScrollView, SafeAreaView } from "react-native";
-import Actions from "../Redux/Actions";
-import Global from "../Styles/GlobalStyles";
+import Constants from "expo-constants";
+import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, Button, Platform } from "react-native";
 
-const Messenger = () => {
-  const [enabled, setEnabled] = React.useState(true);
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) =>
+      setExpoPushToken(token)
+    );
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        setNotification(notification);
+      }
+    );
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+
   return (
-    <SafeAreaView style={Global.Styles.container}>
-      <ScrollView scrollEnabled={enabled}>
-        <View style={{ height: 600, backgroundColor: "violet" }}></View>
-        <View style={{ height: 2000, backgroundColor: "red" }}>
-          <ScrollView
-            pagingEnabled
-            scrollEventThrottle={16}
-            onTouchStart={(ev) => {
-              setEnabled(false);
-            }}
-            onMomentumScrollEnd={(e) => {
-              setEnabled(true);
-            }}
-            onScrollEndDrag={(e) => {
-              setEnabled(true);
-            }}
-            style={{ margin: 10, maxHeight: 200 }}
-          >
-            <View style={{ height: 200, backgroundColor: "blue" }} />
-            <View style={{ height: 200, backgroundColor: "pink" }} />
-            <View style={{ height: 200, backgroundColor: "blue" }} />
-            <View style={{ height: 200, backgroundColor: "pink" }} />
-            <View style={{ height: 200, backgroundColor: "blue" }} />
-            <View style={{ height: 200, backgroundColor: "pink" }} />
-            <View style={{ height: 200, backgroundColor: "blue" }} />
-            <View style={{ height: 200, backgroundColor: "pink" }} />
-            <View style={{ height: 200, backgroundColor: "blue" }} />
-            <View style={{ height: 200, backgroundColor: "pink" }} />
-            <View style={{ height: 200, backgroundColor: "blue" }} />
-            <View style={{ height: 200, backgroundColor: "pink" }} />
-          </ScrollView>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{" "}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{" "}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
+    </View>
   );
-};
+}
 
-export default Messenger;
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
