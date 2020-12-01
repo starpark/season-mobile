dcaacbba56107c46ede3df868c070ff997b932f7
starpark <62285975+starpark@users.noreply.github.com>
2020-12-02 08:44:26 +0900

    add test case

--- App/Screens/Course/CourseQnA.js
+++ App/Screens/Course/CourseQnA.js
@@ -1,29 +1,234 @@
 import * as React from "react";
-import { ScrollView, View, Text, Dimensions } from "react-native";
-import { useRoute } from "@react-navigation/native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  FlatList,
+  RefreshControl,
+} from "react-native";
 import Global from "../../Styles/GlobalStyles";
-
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+import { TouchableRipple } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 const { width: screenWidth } = Dimensions.get("window");
 
-const test = `청춘의 꽃이 그것을 같으며, 인생의 철환하였는가? 눈에 봄바람을 현저하게 힘있다. 가장 오아이스도 가치를 날카로우나 사랑의 바이며, 교향악이다. 자신과 피어나기 두손을 이성은 불어 부패뿐이다. 고행을 그들은 착목한는 시들어 것이다. 그들에게 우리 영락과 같이 생의 것이다. 뛰노는 생의 이것을 있으랴? 밥을 인간이 가슴이 용감하고 싶이 크고 이것이다. 이상은 그들은 그들의 따뜻한 것이다.
-
-있으며, 동력은 속에서 인간의 이성은 곳이 안고, 천하를 노년에게서 그리하였는가? 같지 가슴에 열락의 원질이 용기가 쓸쓸하랴? 이상을 불어 가치를 봄날의 사막이다. 보이는 듣기만 할지라도 바로 있으며, 뿐이다. 청춘은 가치를 오아이스도 열락의 힘있다. 창공에 곧 생명을 오직 열락의 부패뿐이다. 있는 무한한 따뜻한 풍부하게 것이다. 위하여, 그와 열락의 부패뿐이다. 예가 천고에 같으며, 있으랴?
-
-이것이야말로 위하여 실로 살았으며, 오직 수 것이다. 그들은 인간이 피부가 불러 모래뿐일 황금시대를 얼음이 열매를 되려니와, 것이다. 실로 꽃이 더운지라 갑 못할 못하다 대중을 보배를 청춘의 봄바람이다. 대한 그들은 풀이 청춘의 미묘한 사람은 있을 관현악이며, 교향악이다. 풍부하게 노래하며 인도하겠다는 천하를 예수는 청춘이 뼈 관현악이며, 웅대한 황금시대다. 수 그들은 가슴이 앞이 살았으며, 위하여서. 부패를 날카로우나 스며들어 가슴이 운다. 과실이 목숨이 풀밭에 기쁘며, 수 피가 간에 얼음이 있는가? 노래하며 속잎나고, 미인을 할지니, 천고에 얼마나 같이, 관현악이며, 구할 뿐이다.
-
-소리다.이것은 열매를 인생에 싹이 그들은 가치를 품었기 우리는 용기가 힘있다. 찾아다녀도, 청춘의 살았으며, 있다. 불러 장식하는 끝까지 이것이야말로 안고, 공자는 이것이다. 따뜻한 있을 무한한 싶이 광야에서 물방아 방지하는 힘차게 이것이다. 위하여, 있는 풀이 간에 맺어, 운다. 미묘한 이상, 바로 가는 우는 기쁘며, 이상의 있으랴? 반짝이는 뜨고, 때에, 황금시대다. 놀이 뛰노는 주며, 것이다. 희망의 청춘의 스며들어 수 별과 힘있다.
-
-있을 끓는 따뜻한 그들의 그들은 거선의 품으며, 희망의 있는가? 우리 방지하는 따뜻한 것이 우는 위하여 못할 꽃이 얼음 때문이다. 그들의 그들의 청춘을 역사를 피가 길을 봄바람이다. 이상을 풍부하게 투명하되 꽃이 것이다. 굳세게 미묘한 것은 싸인 곳으로 힘차게 못할 이상이 것이다. 같은 타오르고 소금이라 황금시대다. 원대하고, 타오르고 되려니와, 온갖 아니다. 눈에 산야에 꽃 긴지라 피어나는 행복스럽고 옷을 사막이다. 피가 피고 무엇을 고행을 불어 되려니와, 내는 얼마나 사는가 그리하였는가? 반짝이는 피는 설레는 우리 얼마나 황금시대다.`;
+const response = [
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "<span>아령하세여</span>",
+    issuer: "string",
+    createdAt: "2020-11-24T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body:
+      "<a href='https://learn-ap-northeast-2-prod-fleet01-xythos.content.blackboardcdn.com/5cf774ff89eaf/3508111?X-Blackboard-Expiration=1606759200000&X-Blackboard-Signature=O3KAXZyGt3p2s%2FuXSFy6w1VL7AajzG6AY9T2X9oQXJ0%3D&X-Blackboard-Client-Id=513122&response-cache-control=private%2C%20max-age%3D21600&response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%27C1.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20201130T120000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=AKIAZH6WM4PLRAIZCZRQ%2F20201130%2Fap-northeast-2%2Fs3%2Faws4_request&X-Amz-Signature=dd02230831e326427eae01d0a7403fe8be607da03a32e6383b3b1555288ede67'> rr </a>",
+    issuer: "string",
+    createdAt: "2020-11-24T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-24T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-24T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-24T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-23T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-23T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-23T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-23T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-23T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-23T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-22T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title: "교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-22T07:50:45.997Z",
+    updatedAt: "date",
+  },
+  {
+    title:
+      "교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1교수님 질문이요!!!!!!!!!!1",
+    body: "string",
+    issuer: "string",
+    createdAt: "2020-11-10T07:50:45.997Z",
+    updatedAt: "date",
+  },
+];
 
 const CourseQnA = () => {
-  const route = useRoute();
-  console.log(route);
+  const [refreshing, setRefreshing] = React.useState(false);
+  const navigation = useNavigation();
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+  function timeForToday(value) {
+    const today = new Date();
+    const timeValue = new Date(value);
+
+    const betweenTime = Math.floor(
+      (today.getTime() - timeValue.getTime()) / 1000 / 60
+    );
+    if (betweenTime < 1) return "방금전";
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 7) {
+      return `${betweenTimeDay}일전`;
+    }
+
+    var year = timeValue.getFullYear(); //yyyy
+    var month = 1 + timeValue.getMonth(); //M
+    month = month >= 10 ? month : "0" + month; //month 두자리로 저장
+    var day = timeValue.getDate(); //d
+    day = day >= 10 ? day : "0" + day;
+
+    return `${year}-${month}-${day}`;
+  }
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableRipple
+        onPress={() =>
+          navigation.navigate("CourseQnADetail", { item, way: "질의응답" })
+        }
+      >
+        <View style={styles.notice_box}>
+          <View>
+            <Text numberOfLines={1} style={styles.notice_title}>
+              {item.title}
+            </Text>
+            <Text style={{ color: Global.Colors.sjWgray2 }}>
+              {item.issuer} · {timeForToday(item.createdAt)}
+            </Text>
+          </View>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <MaterialCommunityIcons
+              name="message-reply-text"
+              size={16}
+              color={Global.Colors.sjred}
+              style={{ marginHorizontal: 5 }}
+            />
+            <Text>0</Text>
+          </View>
+
+          <Entypo
+            name="chevron-right"
+            size={24}
+            color={Global.Colors.sjgray}
+            style={{ alignSelf: "center" }}
+          />
+        </View>
+      </TouchableRipple>
+    );
+  };
 
   return (
-    <ScrollView>
-      <View></View>
-    </ScrollView>
+    <View style={styles.contanier}>
+      <FlatList
+        data={response}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => `${index}${item.createdAt}`}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  contanier: {
+    flex: 1,
+    paddingBottom: 60,
+  },
+  notice_box: {
+    width: screenWidth,
+    height: 60,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  notice_title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    fontFamily: "Square",
+    width: 250,
+  },
+});
 
 export default CourseQnA;
