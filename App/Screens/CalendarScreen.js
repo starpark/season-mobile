786e54865d7f4f003319b4aa68a17284c81c4b2f
starpark <62285975+starpark@users.noreply.github.com>
2020-12-02 08:41:21 +0900

    add calendar module

--- App/Screens/CalendarScreen.js
+++ App/Screens/CalendarScreen.js
@@ -1,175 +1,246 @@
-import React, { useRef } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  Image,
-  View,
-  Text,
-  Animated,
-} from "react-native";
-import faker from "faker";
+import * as React from "react";
+import { View, Dimensions, Text, ScrollView } from "react-native";
+import { Calendar, LocaleConfig, Agenda } from "react-native-calendars";
+import Global from "../Styles/GlobalStyles";
 
-const HEADER_MAX_HEIGHT = 240;
-const HEADER_MIN_HEIGHT = 84;
-const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
+const { width: screenWidth } = Dimensions.get("window");
 
-const DATA = Array(10)
-  .fill(null)
-  .map((_, idx) => ({
-    id: idx,
-    avatar: faker.image.avatar(),
-    fullName: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  }));
+LocaleConfig.locales["ko"] = {
+  monthNames: [
+    "1월",
+    "2월",
+    "3월",
+    "4월",
+    "5월",
+    "6월",
+    "7월",
+    "8월",
+    "9월",
+    "10월",
+    "11월",
+    "12월",
+  ],
+  monthNamesShort: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+  ],
+  dayNames: [
+    "일요일",
+    "월요일",
+    "화요일",
+    "수요일",
+    "목요일",
+    "금요일",
+    "토요일",
+  ],
+  dayNamesShort: ["일", "월", "화", "수", "목", "금", "토"],
+  today: "오늘",
+};
+LocaleConfig.defaultLocale = "ko";
 
-function App() {
-  const scrollY = useRef(new Animated.Value(0)).current;
-
-  const headerTranslateY = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, -HEADER_SCROLL_DISTANCE],
-    extrapolate: "clamp",
-  });
-
-  const imageOpacity = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
-    outputRange: [1, 1, 0],
-    extrapolate: "clamp",
-  });
-  const imageTranslateY = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, 100],
-    extrapolate: "clamp",
-  });
-
-  const titleScale = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
-    outputRange: [1, 1, 0.9],
-    extrapolate: "clamp",
-  });
-  const titleTranslateY = scrollY.interpolate({
-    inputRange: [0, HEADER_SCROLL_DISTANCE / 2, HEADER_SCROLL_DISTANCE],
-    outputRange: [0, 0, -8],
-    extrapolate: "clamp",
-  });
-
-  const renderListItem = (item) => (
-    <View key={item.id} style={styles.card}>
-      <Image style={styles.avatar} source={{ uri: item.avatar }} />
-      <Text style={styles.fullNameText}>{item.fullName}</Text>
-    </View>
-  );
+const CalendarScreen = () => {
+  const [pickDate, setPickDate] = React.useState({});
 
   return (
-    <SafeAreaView style={styles.saveArea}>
-      <Animated.ScrollView
-        contentContainerStyle={{
-          paddingTop: HEADER_MAX_HEIGHT,
-          paddingBottom: 80,
+    <View style={[Global.Styles.container, { backgroundColor: "#F5F5F5" }]}>
+      <Text
+        style={{
+          fontFamily: "Square",
+          fontSize: 25,
+          marginLeft: 20,
+          marginTop: 20,
+          elevation: 10,
+          zIndex: 10,
         }}
-        scrollEventThrottle={16}
-        onScroll={Animated.event(
-          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-          { useNativeDriver: true }
-        )}
       >
-        {DATA.map(renderListItem)}
-      </Animated.ScrollView>
-      <Animated.View
-        style={[
-          styles.header,
-          { transform: [{ translateY: headerTranslateY }] },
-        ]}
-      >
-        <Animated.Image
-          style={[
-            styles.headerBackground,
-            {
-              opacity: imageOpacity,
-              transform: [{ translateY: imageTranslateY }],
-            },
-          ]}
-          source={require(`../Assets/image/background1.jpg`)}
-        />
-      </Animated.View>
-      <Animated.View
-        style={[
-          styles.topBar,
-          {
-            transform: [{ scale: titleScale }, { translateY: titleTranslateY }],
+        캘린더
+      </Text>
+      <View
+        style={{
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          overflow: "hidden",
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
           },
-        ]}
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+
+          elevation: 5,
+          backgroundColor: "white",
+          position: "absolute",
+          top: 0,
+          right: 0,
+          left: 0,
+          paddingTop: 40,
+          zIndex: 9,
+        }}
       >
-        <Text style={styles.title}>Management</Text>
-      </Animated.View>
-    </SafeAreaView>
+        <Calendar
+          style={{ marginVertical: 30 }}
+          calendarWidth={screenWidth - 20}
+          markedDates={{
+            "2020-12-14": {
+              periods: [
+                {
+                  startingDay: true,
+                  endingDay: true,
+                  color: Global.Colors.course[0],
+                },
+                {
+                  startingDay: true,
+                  endingDay: true,
+                  color: Global.Colors.course[1],
+                },
+                {
+                  startingDay: true,
+                  endingDay: false,
+                  color: Global.Colors.course[2],
+                },
+              ],
+            },
+            "2020-12-15": {
+              periods: [
+                { startingDay: true, endingDay: true, color: "#ffa500" },
+                { color: "transparent" },
+                {
+                  startingDay: false,
+                  endingDay: true,
+                  color: Global.Colors.course[2],
+                },
+              ],
+            },
+          }}
+          monthFormat={"yy년 MM월"}
+          markingType="multi-period"
+          onDayPress={(e) => {
+            setPickDate(e);
+          }}
+        />
+      </View>
+      {pickDate.timestamp && (
+        <ScrollView
+          contentContainerStyle={{
+            paddingTop: 400,
+            paddingBottom: 60,
+            paddingHorizontal: 20,
+          }}
+        >
+          <View>
+            <Text
+              style={{ fontFamily: "Square", fontWeight: "bold", fontSize: 25 }}
+            >
+              {pickDate.month}월 {pickDate.day}일
+            </Text>
+            <View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  height: 100,
+                  marginVertical: 7,
+                  backgroundColor: "white",
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 1,
+                  },
+                  shadowOpacity: 0.2,
+                  shadowRadius: 1.41,
+
+                  elevation: 2,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: Global.Colors.course[0],
+                    width: 3,
+                  }}
+                />
+                <View style={{ padding: 5 }}>
+                  <Text style={{ fontFamily: "Square", fontSize: 17 }}>
+                    알고리즘및실습 (001)
+                  </Text>
+                  <Text>과제1</Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  height: 100,
+                  marginVertical: 7,
+                  backgroundColor: "white",
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 1,
+                  },
+                  shadowOpacity: 0.2,
+                  shadowRadius: 1.41,
+
+                  elevation: 2,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: Global.Colors.course[1],
+                    width: 3,
+                  }}
+                />
+                <View style={{ padding: 5 }}>
+                  <Text style={{ fontFamily: "Square", fontSize: 17 }}>
+                    알고리즘및실습 (002)
+                  </Text>
+                  <Text>시험1</Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  height: 100,
+                  marginVertical: 7,
+                  backgroundColor: "white",
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 1,
+                  },
+                  shadowOpacity: 0.2,
+                  shadowRadius: 1.41,
+
+                  elevation: 2,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: Global.Colors.course[2],
+                    width: 3,
+                  }}
+                />
+                <View style={{ padding: 5 }}>
+                  <Text style={{ fontFamily: "Square", fontSize: 17 }}>
+                    알고리즘및실습 (003)
+                  </Text>
+                  <Text>영상1</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  saveArea: {
-    flex: 1,
-    backgroundColor: "#eff3fb",
-  },
-  card: {
-    flexDirection: "row",
-    alignItems: "center",
-    shadowColor: "#402583",
-    backgroundColor: "#ffffff",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 6,
-    elevation: 1,
-    borderRadius: 10,
-    marginHorizontal: 12,
-    marginTop: 12,
-    paddingHorizontal: 16,
-    paddingVertical: 12,
-  },
-  header: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: "#62d1bc",
-    overflow: "hidden",
-    height: HEADER_MAX_HEIGHT,
-  },
-  headerBackground: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    width: null,
-    height: HEADER_MAX_HEIGHT,
-    resizeMode: "cover",
-  },
-  topBar: {
-    marginTop: 40,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-  },
-  title: {
-    color: "white",
-    fontSize: 20,
-  },
-  avatar: {
-    height: 54,
-    width: 54,
-    resizeMode: "contain",
-    borderRadius: 54 / 2,
-  },
-  fullNameText: {
-    fontSize: 16,
-    marginLeft: 24,
-  },
-});
-
-export default App;
+export default CalendarScreen;
